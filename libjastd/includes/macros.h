46436e72d7bcc4c883f36401a06c47ed936efbbd
jas31415 <yourdankestmeme@gmail.com>
2025-06-06 17:09:47 +0200

    renamed the internal macros for vararging

--- libjastd/includes/macros.h
+++ libjastd/includes/macros.h
@@ -36,13 +36,13 @@
 	#define TYPEDEF_GENERIC(type) typedef type##98 type;
 	
 	// @brief  A macro that automates the upsetting variadic syntax
-	#define VARARGS(...) PP_NARG(__VA_ARGS__), ##__VA_ARGS__
+	#define VARARGS(...) _NARG(__VA_ARGS__), ##__VA_ARGS__
 
-	#define PP_NARG(...) \
-        PP_NARG_(__VA_ARGS__, PP_RSEQ_N())
-	#define PP_NARG_(...) \
-		PP_ARG_N(__VA_ARGS__)
-	#define PP_ARG_N( \
+	#define _NARG(...) \
+        _NARG_(__VA_ARGS__, _RSEQ_N())
+	#define _NARG_(...) \
+		_ARG_N(__VA_ARGS__)
+	#define _ARG_N( \
 		_1, _2, _3, _4, _5, _6, _7, _8, _9,_10, \
 		_11,_12,_13,_14,_15,_16,_17,_18,_19,_20, \
 		_21,_22,_23,_24,_25,_26,_27,_28,_29,_30, \
@@ -50,7 +50,7 @@
 		_41,_42,_43,_44,_45,_46,_47,_48,_49,_50, \
 		_51,_52,_53,_54,_55,_56,_57,_58,_59,_60, \
 		_61,_62,_63,N,...) N
-	#define PP_RSEQ_N() \
+	#define _RSEQ_N() \
 		63,62,61,60,                   \
 		59,58,57,56,55,54,53,52,51,50, \
 		49,48,47,46,45,44,43,42,41,40, \
