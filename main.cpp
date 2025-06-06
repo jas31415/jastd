edb5290fa196d01d4b88d3862488924ee7ab3926
jas31415 <yourdankestmeme@gmail.com>
2025-06-06 05:42:00 +0200

    implemented new string features and rewrote flagging section

--- main.cpp
+++ main.cpp
@@ -2,15 +2,14 @@
 	main.cpp is the file where unit tests are performed on the wrapper classes
 
 	note for developers
-		since this program is not complicated but needs to compile in all standards
-		i advise you to restrict yourself to C++98 features, and deal with deprecations using pre-processor statements
+		this program is not complicated but needs to compile in all standards
+		thus i advise you to restrict yourself to C++98 features, and deal with deprecations using pre-processor statements
 		i will leave you with a good resource for C++98 here: https://cplusplus.com/doc/oldtutorial/
 
 	despite the library itself being formatted to match the standard library's style,
 	the unit testing part of this program will be using recommended C++ formatting
 */
 #include <iostream>
-#include <vector>
 
 #include "string.h"
 
@@ -51,31 +50,9 @@ int main()
 		"\tstd\t\t\t\t\t\tShows the C++ Standard version currently being used\n"
 		"\tjastd\t\t\t\t\t\tShows the jastd version currently being used\n"
 		"\thelp\t\t\t\t\t\tShows this menu\n"
-		"\tquit\t\t\t\t\t\tcloses the program\n";
-
+		"\tquit\t\t\t\t\t\tCloses the program\n";
+	
 	std::cout << welcomeMessage << helpMessage << std::endl;
-
-	/*
-		a list of valid inputs
-		[0] "-e", "--everything",
-		[2] "-a", "--available",
-		[4] "-s", "--select",
-		[6] "test", "list",
-		[8] "std", "jastd",
-		[10] "help", "quit"
-		(i promise this will change later)
-	*/
-	const string registeredInputs[] =
-	{
-		"-e", "--everything",
-		"-a", "--available",
-		"-s", "--select",
-		"test", "list",
-		"std", "jastd",
-		"help", "quit"
-	};
-	// use jastd::vector<string> once subset() is developed
-	// const std::vector<string> registeredInputs(_regsTemp, _regsTemp + 2);
 
 	// using ArgumentFlags namespace for readability's sake
 	using namespace ArgumentFlags;
@@ -85,9 +62,6 @@ int main()
 	{
 		// string container for the command fetched from user input
 		string command = "";
-
-		// the maximum argument length is 18 because the longest valid character sequence is "condition_variable", any argument longer than this would be invalid
-		// const int MAX_ARGUMENT_LENGTH = 18;
 
 		// index where the last argument stopped, 0 if a new argument is being parsed
 		size_t previousIndex = 0;
@@ -106,54 +80,43 @@ int main()
 			string argument = command.substr(' ', previousIndex);
 			previousIndex += argument.size() + 1;
 
-			// set argument flags (please find a prettier way to do this lol)
-			if (argument.match_any(registeredInputs[0], 2))
-			{
-				argumentFlags |= EVERYTHING;
-			}
-			else if (argument.match_any(registeredInputs[2], 2))
-			{
-				argumentFlags |= AVAILABLE;
-			}
-			else if (argument.match_any(registeredInputs[4], 2))
-			{
-				argumentFlags |= SELECT;
-			}
-			else if (argument.match(registeredInputs[6]))
-			{
-				argumentFlags |= TEST;
-			}
-			else if (argument.match(registeredInputs[7]))
-			{
-				argumentFlags |= LIST;
-			}
-			else if (argument.match(registeredInputs[8]))
-			{
-				argumentFlags |= STANDARD_VERSION;
-			}
-			else if (argument.match(registeredInputs[9]))
-			{
-				argumentFlags |= JASTD_VERSION;
-			}
-			else if (argument.match(registeredInputs[10]))
-			{
-				argumentFlags |= HELP;
-			}
-			else if (argument.match(registeredInputs[11]))
-			{
-				argumentFlags |= QUIT;
-			}
-			else
+			// set argument flags
+			argumentFlags |=
+				(argument.match_any("-e", "--everything"))
+					? EVERYTHING :
+				(argument.match_any("-a", "--available"))
+					? AVAILABLE :
+				(argument.match_any("-s", "--select"))
+					? SELECT :
+				(argument.match("test"))
+					? TEST :
+				(argument.match("list"))
+					? LIST :
+				(argument.match("std"))
+					? STANDARD_VERSION :
+				(argument.match("jastd"))
+					? JASTD_VERSION :
+				(argument.match("help"))
+					? HELP :
+				(argument.match("quit"))
+					? QUIT :
+						CLEAR; // the flags clear when an invalid argument occured...
+			
+			// ...and thus it prints error message to console output
+			if (argumentFlags == CLEAR)
 			{
 				std::cerr << "'" << argument << "' in '" << command << "' is not a recognized argument. Write 'help' to see a list of commands." << std::endl;
+				break;
 			}
 		}
 
 		switch (argumentFlags)
-		{			
+		{
 			case QUIT:
 				quit = true;
 			break;
+			// case TEST | EVERYTHING:
+			// break;
 		}
 	}
 
